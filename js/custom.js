998aaa9ba380acd9d29990bb46316e86093b95f4
garlatonic <sangapark34@naver.com>
2022-04-06 20:26:15 +0900

    0406

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,47 @@
 $(function(){
-  new TypeIt("#thisis", {
-    strings: ["This is", "IVORY's", "PORTFOLIO."],
-    speed: 100,
-    waitUntilVisible: false,
-  }).go();
+  /* --------- Header --------- */
+  /* --------- Skills : Chart animation --------- */
+  var chart = $('.chart');
+  var skill = $ ('.about-skills');
+  var skillOff = skill.offset().top - 700;
+  var excuted = false;
+  var windowHeight = $(window).innerHeight();
+
+  $(window).scroll(function(){
+    var windowSCR = $(this).scrollTop();
+    if(windowSCR > skillOff){ // Chart Animation
+      if(excuted == false){
+        animateChart();
+        excuted = true;
+      };
+      console(excuted);
+    }else if(windowSCR > windowHeight){ // Header Active
+      $('header').addClass('active');
+    }else { // Clear
+      $('header').removeClass('active');
+      excuted = false;
+    };
+  });
+
+
+
+  function animateChart(){
+    chart.each(function(){
+      var item = $(this);
+      var title = item.find('h3');
+      var targetNum = title.attr('data-num');
+      var circle = item.find('circle');
+  
+      $({rate:0}).animate({rate:targetNum},
+        {
+          duration: 1500,
+          progress: function(){
+            var now = this.rate;
+            var amount = 565 - (565 * now / 100);
+            title.text(Math.floor(now)+"%");
+            circle.css({strokeDashoffset:amount});
+          }
+      });
+    }); //chart.each
+  }
 });
